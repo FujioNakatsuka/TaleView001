6da511d8204046e7cabf7408ecd3e915a9dfe358
Fujio Nakatsuka <com.nifty.cbm01799>
2020-06-03 17:57:08 +0900

    Initial Commit

---
+++ TaleView001/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaleView001
+//
+//  Created by 中塚富士雄 on 2020/06/03.
+//  Copyright © 2020 中塚富士雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
